4272eeb0b414a02b7ddbfefda27d11ed8c22b5ad
cooolinho <coding@cooolinho.de>
2021-03-14 15:42:55 +0100

    add trainer-dashboard

---
+++ symfony/src/Controller/Trainer/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Trainer;
+
+use App\Entity\Player;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/trainer/dashboard", name="admin_trainer_dashboard")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('TSV Squad Planner');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToRoute('My Players', 'fas fa-list', 'trainer_player_index');
+    }
+}
