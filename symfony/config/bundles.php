7127c6fca63438e1556b03e5ad45023236696bf1
cooolinho <coding@cooolinho.de>
2021-03-17 22:59:44 +0100

    add symfony-security-bundle

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -16,4 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Cooolinho\Bundle\FileImporterBundle\FileImporterBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Cooolinho\SecurityBundle\CooolinhoSecurityBundle::class => ['all' => true],
 ];
